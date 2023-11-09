a757ab61d7f1516976ecea75d6f6974401d35dd5
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-11-09 17:57:38 +0100

    Initial commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+// import './Navbar.css';
+
+const Navbar = () => (
+  <div className="text-center text-xl font-bold text-yellow-500">Navbar</div>
+);
+
+export default Navbar;
