033528d51972461a37b9c9e2cdc65c41f694d38c
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-11-11 22:18:19 +0100

    Styled component

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,7 +1,15 @@
+import {GiHamburgerMenu} from "react-icons/md"
+
 function Button({ children }) {
   return (
-    <button className="bg-crimson font rounded-1px cursor-pointer border-none px-6 py-2 text-base font-bold leading-7 tracking-wider text-black outline-none">
+    <button
+      className="bg-crimson font rounded-1px 
+    cursor-pointer border-none px-6 py-2 text-base 
+    font-bold leading-7 tracking-wider text-black 
+    outline-none lg:text-4xl lg:leading-[67px]"
+    >
       {children}
+      <GiHamburgerMenu/>
     </button>
   );
 }
