a64a941f4a73344cf8a684a2a06d2b2f35e7051d
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-11-11 22:18:11 +0100

    Navbar styling

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+// import {GiHamburgerMenu} from "react-icons/md"
 
 // import {GiHamburgerMenu} from "react-icons/gi"
 // import './Navbar.css';
 
 const Navbar = () => (
-  <div className="text-center text-xl font-bold text-golden">Navbar</div>
+  <div className="text-golden text-center text-xl font-bold">
+    Navbar
+    {/* <HiOutlineUser/> */}
+    </div>
+    
 );
 
 export default Navbar;
