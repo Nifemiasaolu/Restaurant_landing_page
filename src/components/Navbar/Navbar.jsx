8ef8ffae861bf75cfc8ae32a0a6f84988d1267d0
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-11-11 12:30:55 +0100

    Style on navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ import React from "react";
 // import './Navbar.css';
 
 const Navbar = () => (
-  <div className="text-center text-xl font-bold text-yellow-500">Navbar</div>
+  <div className="text-center text-xl font-bold text-golden">Navbar</div>
 );
 
 export default Navbar;
