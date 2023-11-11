35dfaf85863e3264349cc1c3cfab30beae1662b9
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-11-11 12:30:31 +0100

    Styles added

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ import { Navbar } from "./components";
 import "./App.css";
 
 const App = () => (
-  <div>
+  <div className = "app-bg app-wrapper">
     <Navbar />
     <Header />
     <AboutUs />
