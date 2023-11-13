167c51a229e1231b78e959919326140d73c3d261
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-11-13 15:04:19 +0100

    Reusable syle

---
+++ src/ui/AnchorTag.jsx
@@ -0,0 +1,14 @@
+function AnchorTag({ children, href }) {
+  const className =
+    `ease-in-out hover:border-b-solid mx-4 
+    my-0 no-underline transition-all duration-300 
+    hover:border-b-[1px] hover:border-b-white`;
+
+  return (
+    <a href={href} className={className}>
+      {children}
+    </a>
+  );
+}
+
+export default AnchorTag;
