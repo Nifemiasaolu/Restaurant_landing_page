50b9c3e3f5ec5127f0762068ea2875639a6ebbe1
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-11-13 15:02:29 +0100

    Tweak in styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 
 // eslint-disable-next-line
- export default {
+export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     fontFamily: {
@@ -9,9 +9,9 @@
         '"Cormorant Upright, serif"',
         '"Open Sans, sans-serif"',
         {
-          fontFeatureSettings: '"tnum", "lnum"'
-        }
-      ]
+          fontFeatureSettings: '"tnum", "lnum"',
+        },
+      ],
     },
 
     extend: {
@@ -20,17 +20,25 @@
       // },
 
       colors: {
-        "transparent": "transparent",
-        "current": "currentColor",
-        "golden": "#DCCA87",
-        "black": "#0C0C0C",
-        "gray": "#545454",
-        "crimson": " #F5EFDB",
-        "lightGrey": "#AAAAAA",
-        "white": "#ffffff",
-      }
+        transparent: "transparent",
+        current: "currentColor",
+        golden: "#DCCA87",
+        black: "#0C0C0C",
+        gray: "#545454",
+        crimson: " #F5EFDB",
+        lightGrey: "#AAAAAA",
+        white: "#ffffff",
+      },
 
-  
+      height: {
+        screen: "100dvh",
+      },
+
+      // a: {
+      //   "color": "unset",
+      //   "text-decoration": "none"
+      // }
+
     },
     // screens: {
     //   "tablet": "650px",
