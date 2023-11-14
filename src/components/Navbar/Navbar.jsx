c7d6640cc950cfe4f145c2f0443632fceba10723
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-11-14 18:12:32 +0100

    Details added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,51 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { GiHamburgerMenu } from "react-icons/gi";
 import { MdOutlineRestaurantMenu } from "react-icons/md";
 // import {MdOutlineRestaurantMenu}
-import images from "../../constants/images";
+import {images} from "../../constants";
 import NavbarItem from "./NavbarItem";
 import { menus } from "../../constants/data";
 import AnchorTag from "../../ui/AnchorTag";
 
-const Navbar = () => (
-  <nav
-    className="flex w-full items-center 
-    justify-between bg-black px-8 py-4"
-  >
-    <div className="flex items-center justify-start">
-      <img src={images.gericht} alt="app logo" className="w-[150px]" />
-    </div>
+const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
 
-    <ul className="flex-center flex-1">
-      {menus.map((item) => (
-        <NavbarItem item={item} key={item.id} />
-      ))}
-    </ul>
-
-    <div className="p-opensans flex items-center justify-end ">
-      <AnchorTag href="#login">Login / Register</AnchorTag>
-
-      <div className="h-[30px] w-[1px] bg-lightGrey" />
-
-      <AnchorTag href="/">Book Table</AnchorTag>
-    </div>
-
-    <div className="sm:hidden">
-      <GiHamburgerMenu className="text-white" onClick={() => {}} />
-
-      <div className="flex-center slide-bottom">
-        <MdOutlineRestaurantMenu className="text-white" onClick={() => {}} />
-
-        <ul className="">
-          {menus.map((item) => (
-            <NavbarItem item={item} key={item.id} />
-          ))}
-        </ul>
+  return (
+    <nav
+      className="flex w-full items-center 
+      justify-between bg-black px-8 py-4"
+    >
+      <div className="flex items-center justify-start">
+        <img src={images.gericht} alt="app logo" className="w-[100px] sm-[200px]" />
       </div>
-    </div>
-  </nav>
-);
+
+      <ul className="flex-center hidden flex-1 sm:flex">
+        {menus.map((item) => (
+          <NavbarItem item={item} key={item.id} />
+        ))}
+      </ul>
+      <div className="p-opensans hidden items-center justify-end sm:flex">
+        <AnchorTag href="#login">Login / Register</AnchorTag>
+
+        <div className="h-[30px] w-[1px] bg-lightGrey" />
+
+        <AnchorTag href="/">Book Table</AnchorTag>
+      </div>
+
+      {/* Menu Toggle */}
+      <div className="flex sm:hidden">
+        <GiHamburgerMenu
+          className="text-white cursor-pointer"
+          onClick={() => setToggleMenu(true)}
+        />
+
+        {toggleMenu && (
+          <div
+            className="flex-center slide-bottom fixed 
+          left-0 top-0 z-[5] h-screen w-full flex-col 
+          bg-black transition duration-300 ease-in-out"
+          >
+            <MdOutlineRestaurantMenu
+              className="absolute right-5 top-5 cursor-pointer 
+              text-[27px] text-golden "
+              onClick={() => setToggleMenu(false)}
+            />
+
+            <ul className="">
+              {menus.map((item) => (
+                <NavbarItem item={item} key={item.id} />
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
 
