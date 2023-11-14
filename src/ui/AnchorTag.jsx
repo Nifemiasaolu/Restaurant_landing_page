c437b76ef03b59ec9748ef1d709a4d1cb6ec11ba
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-11-14 18:13:47 +0100

    Lil  style change

--- src/ui/AnchorTag.jsx
+++ src/ui/AnchorTag.jsx
@@ -1,7 +1,7 @@
 function AnchorTag({ children, href }) {
-  const className = `ease-in-out hover:border-b-solid mx-4 my-0 no-underline 
+  const className = `ease-in-out hover:border-b-solid mx-4 no-underline 
     transition-all duration-300 hover:border-b-[1px] 
-    hover:border-b-white`;
+    hover:border-b-golden sm:text-xs lg:text-sm md:mr-2`;
 
   return (
     <a href={href} className={className}>
