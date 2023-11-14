164db7f3134c30bb8d68b7ed16d1666d488e3925
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-11-14 18:12:48 +0100

    Lil tweak on style

--- src/components/Navbar/NavbarItem.jsx
+++ src/components/Navbar/NavbarItem.jsx
@@ -1,9 +1,13 @@
-function NavbarItem({item}) {
+function NavbarItem({ item }) {
   return (
-    <li className="p-opensans hover:text-lightGrey mr-4">
+    <li
+      className="p-opensans m-4 cursor-pointer font-['Cormorant_Upright']
+     text-golden hover:text-white sm:mr-2 sm:font-['Open_sans']
+      sm:text-white sm:hover:text-lightGrey sm:text-xs lg:text-sm"
+    >
       <a href={item.id}>{item.title}</a>
     </li>
-  )
+  );
 }
 
-export default NavbarItem
+export default NavbarItem;
