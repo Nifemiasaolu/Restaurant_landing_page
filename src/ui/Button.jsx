eadf6a88227550af50cb1a4f76ae92d1ef00a050
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-11-14 18:13:57 +0100

    Lil style change

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -2,13 +2,12 @@
 function Button({ children }) {
   return (
     <button
-      className="bg-crimson font rounded-1px 
+      className="bg-crimson font-['Cormont_Upright'] rounded-[4px] 
     cursor-pointer border-none px-6 py-2 text-base 
     font-bold leading-7 tracking-wider text-black 
-    outline-none lg:text-4xl lg:leading-[67px]"
+    outline-none lg:text-xl lg:leading-[50px]"
     >
       {children}
-      {/* <GiHamburgerMenu/> */}
       
     </button>
   );
