c0df08a8d8dee0f0333825ec9007698fe0ad09eb
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-11-14 18:13:10 +0100

    Subheading detail

--- src/components/SubHeading/SubHeading.jsx
+++ src/components/SubHeading/SubHeading.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 
-const SubHeading = () => (
-  <div>
-    SubHeading
+import { images } from '../../constants';
+
+const SubHeading = ({title}) => (
+  <div className='mb-4'>
+    <p className='p-cormorant'>{title}</p>
+    <img src={images.spoon} alt="spoon" className='spoon-img'/>
   </div>
 );
 
