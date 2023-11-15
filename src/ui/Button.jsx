4685652539c560d2224956adc51941e394cd12b2
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-11-15 19:48:08 +0100

    Reusable button

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,14 +1,22 @@
 
-function Button({ children }) {
+function Button({ children, type }) {
+  const base = `bg-golden font-['Cormont_Upright'] rounded-[4px] 
+  cursor-pointer border-none  text-base 
+  font-bold leading-7 tracking-wider text-black 
+  outline-none `
+
+  const styles= {
+    primary: base + `px-6 py-2 lg:text-xl lg:leading-[50px]`,
+    // small: 
+  } 
+
+  // const type = ""
+
   return (
     <button
-      className="bg-crimson font-['Cormont_Upright'] rounded-[4px] 
-    cursor-pointer border-none px-6 py-2 text-base 
-    font-bold leading-7 tracking-wider text-black 
-    outline-none lg:text-xl lg:leading-[50px]"
+      className={styles[type]}
     >
       {children}
-      
     </button>
   );
 }
