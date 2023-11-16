be5db8aa24775731f049369dcc17440f52c93fdc
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-11-16 16:13:12 +0100

    Page content

--- src/ui/PageContent.jsx
+++ src/ui/PageContent.jsx
@@ -1,19 +1,20 @@
-import { SubHeading } from "../components"
 
 import { images } from "../constants"
-import Button from "./Button"
 
 function PageContent({title, children, styleType}) {
+const base= ` w-[10rem] sm:w-[20rem]`
+  
+
   const styles= {
-    end: `flex justify-end items-end text-right flex-1 flex-col`,
-    normal: ``
+    end:`flex justify-end items-end text-right flex-1 flex-col w-[10rem] `,
+    normal:`w-[10rem] `
   }
 
    return(
     <div className= {styles[styleType]}>
-      <h3 className="p-cormorant">{title}</h3>
+      <h3 className="p-cormorant text-golden">{title}</h3>
       <img src={images.spoon} alt="spoon img" className="spoon-img mb-4"/>
-      <p className="text-white text-xs md:text-sm">{children}</p>
+      <p className="text-lightGrey text-xs base">{children}</p>
     </div>
   )
 }
