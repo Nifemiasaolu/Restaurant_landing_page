21ccd7769245bea54a4a8f3a496f0aaf37e94a69
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-11-16 16:09:18 +0100

    About us page

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -6,7 +6,7 @@ import PageContent from "../../ui/PageContent";
 import Button from "../../ui/Button";
 
 const AboutUs = () => (
-  <div className=" section-padding app-bg flex-center relative h-screen flex-shrink">
+  <div className=" section-padding app-bg flex-center relative h-screen">
     <div>
       <img
         src={images.G}
@@ -15,9 +15,41 @@ const AboutUs = () => (
       />
     </div>
 
-    <div className="flex-center absolute  w-[40rem] gap-4">
-      <div className="flex w-[25rem] flex-col items-end justify-end">
-        <PageContent title="About Us" styleType="end">
+    <div className="flex-center absolute gap-4">
+      <div className="flex flex-1 items-end justify-end text-right flex-col">
+        <h3 className="p-cormorant text-golden">About Us</h3>
+        <img src={images.spoon} alt="spoon img" className="spoon-img mb-4" />
+        <p className="text-xs text-lightGrey w-[10rem] sm:w-[20rem]">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pharetra
+          adipiscing ultrices vulputate posuere tristique. In sed odio nec
+          aliquet eu proin mauris et
+        </p>
+        <Button type="small">Read More</Button>
+      </div>
+
+      <div>
+        <img
+          src={images.knife}
+          alt="knife"
+          className="h-[25rem] mx-2 w-[3rem] sm:h-[25rem] sm:w-[3rem]"
+        />
+      </div>
+
+      <div className="flex-1 flex items-start justify-end text-left flex-col">
+        <h3 className="p-cormorant text-golden">Our History</h3>
+        <img src={images.spoon} alt="spoon img" className="spoon-img mb-4" />
+        <p className="text-xs text-lightGrey  w-[10rem] sm:w-[20rem]">
+          Adipiscing tempus ullamcorper lobortis odio tellus arcu volutpat.
+          Risus placerat morbi volutpat habitasse interdum mi aliquam In sed
+          odio nec aliquet
+        </p>
+        <Button type="small">Read More</Button>
+      </div>
+    </div>
+
+    {/* <div className="flex-center absolute  gap-4">
+      <div className="flex flex-1 items-end justify-end text-right flex-col">
+        <PageContent title="About Us" >
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pharetra
           adipiscing ultrices vulputate posuere tristique. In sed odio nec
           aliquet eu proin mauris et{" "}
@@ -29,19 +61,19 @@ const AboutUs = () => (
         <img
           src={images.knife}
           alt="knife"
-          className="h-[20rem] w-[3rem] sm:h-[25rem] sm:w-[4rem]"
+          className="h-[25rem] mx-2 w-[3rem] sm:h-[30rem] sm:w-[4rem]"
         />
       </div>
 
-      <div className="w-[25rem]">
-        <PageContent title="Our History">
+      <div className="flex-1 flex items-start justify-end text-left flex-col">
+        <PageContent title="Our History" >
           Adipiscing tempus ullamcorper lobortis odio tellus arcu volutpat.
           Risus placerat morbi volutpat habitasse interdum mi aliquam In sed
           odio nec aliquet
         </PageContent>
         <Button type="small">Read More</Button>
       </div>
-    </div>
+    </div> */}
   </div>
 );
 
