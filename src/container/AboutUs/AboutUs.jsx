ff6edca96a5ffc701342d1d59cf6309ea85a6220
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-11-16 10:10:03 +0100

    About us sdetail

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -3,44 +3,43 @@ import React from "react";
 import { images } from "../../constants";
 import { SubHeading } from "../../components";
 import PageContent from "../../ui/PageContent";
-import Button from "../../ui/Button"
-
+import Button from "../../ui/Button";
 
 const AboutUs = () => (
-  <div className="section-padding app-bg flex-center relative">
+  <div className=" section-padding app-bg flex-center relative h-screen flex-shrink">
     <div>
       <img
         src={images.G}
         alt="G"
-        className="flex-center w-[200px] sm:w-[391px] z-0"
+        className="flex-center z-0 w-[150px] sm:w-[251px]"
       />
     </div>
 
-    <div className="flex-center absolute inset-0 w-[50rem] gap-4">
-      <div className="w-[25rem]">
-        <PageContent title="About Us" button="Read More">
+    <div className="flex-center absolute  w-[40rem] gap-4">
+      <div className="flex w-[25rem] flex-col items-end justify-end">
+        <PageContent title="About Us" styleType="end">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pharetra
           adipiscing ultrices vulputate posuere tristique. In sed odio nec
           aliquet eu proin mauris et{" "}
         </PageContent>
-        <Button type="primary">Read More</Button>
+        <Button type="small">Read More</Button>
       </div>
 
       <div>
         <img
           src={images.knife}
           alt="knife"
-          className="h-[20rem] w-[5rem] sm:w-[5rem] sm:h-[25rem]"
+          className="h-[20rem] w-[3rem] sm:h-[25rem] sm:w-[4rem]"
         />
       </div>
 
       <div className="w-[25rem]">
-        <PageContent title="Our History" button="Read More">
+        <PageContent title="Our History">
           Adipiscing tempus ullamcorper lobortis odio tellus arcu volutpat.
           Risus placerat morbi volutpat habitasse interdum mi aliquam In sed
           odio nec aliquet
         </PageContent>
-        <Button type="primary">Read More</Button>
+        <Button type="small">Read More</Button>
       </div>
     </div>
   </div>
