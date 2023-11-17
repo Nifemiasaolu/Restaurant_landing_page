19224cd7ba9ff5536f9f27b7ba4320e35f4cef8d
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-11-17 10:12:58 +0100

    Special menu list

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -1,23 +1,39 @@
 import React from "react";
 
 import "./SpecialMenu.css";
-import { images } from "../../constants";
+import { images, data } from "../../constants";
+import { SubHeading } from "../../components";
+import WineItem from "./WineItem";
 
 const SpecialMenu = () => {
   const today = `Today's Special`;
 
   return (
-    <div className="section-padding flex-center h-screen bg-black">
-      <div className="flex-center flex-col">
+    <div
+      id="menu"
+      className="section-padding flex-center h-screen flex-col bg-black"
+    >
+      {/* <div className="flex-center flex-col">
         <p className="text-xs capitalize text-lightGrey">
           Menu that fits you paatte
         </p>
         <img src={images.spoon} alt="Spoon" className="spoon-img mb-3 mt-1" />
         <h3 className="p-cormorant text-golden">{today}</h3>
-      </div>
+      </div> */}
+
+      <SubHeading title="Menu that fits you palatte" type="flex">
+        {today}
+      </SubHeading>
 
       <div>
-        <h4>Wine & Beer</h4>
+        <div>
+          <h4 className="text-white">Wine & Beer</h4>
+          <ul>
+            {data.wines.map((wine, index) => (
+              <WineItem key={wine.item} wine={wine} />
+            ))}
+          </ul>
+        </div>
       </div>
     </div>
   );
