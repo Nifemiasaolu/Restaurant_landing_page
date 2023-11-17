8658433e600d84f14b183a10a0d41019b56f2410
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-11-17 10:13:19 +0100

    Wine item list

---
+++ src/container/Menu/WineItem.jsx
@@ -0,0 +1,15 @@
+function WineItem({ wine }) {
+  return (
+    <li>
+      <div className="flex-center">
+        <p className="text-golden">{wine.title}</p>
+        <span className="mx-2 text-lightGrey">-------</span>
+        <small className="text-white">{wine.price}</small>
+      </div>
+
+      <div className="text-lightGrey">{wine.tags}</div>
+    </li>
+  );
+}
+
+export default WineItem;
