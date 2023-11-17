c4638d5d8e06afe32ca6aa3f0dc8bd771bd8306d
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-11-17 10:11:47 +0100

    About us

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 
 import { images } from "../../constants";
-import { SubHeading } from "../../components";
-import PageContent from "../../ui/PageContent";
 import Button from "../../ui/Button";
 
 const AboutUs = () => (
-  <div className=" section-padding app-bg flex-center relative h-screen">
+  <div
+    id="about"
+    className=" section-padding app-bg flex-center relative h-screen"
+  >
     <div>
       <img
         src={images.G}
@@ -16,10 +17,10 @@ const AboutUs = () => (
     </div>
 
     <div className="flex-center absolute gap-4">
-      <div className="flex flex-1 items-end justify-end text-right flex-col">
+      <div className="flex flex-1 flex-col items-end justify-end text-right">
         <h3 className="p-cormorant text-golden">About Us</h3>
         <img src={images.spoon} alt="spoon img" className="spoon-img mb-4" />
-        <p className="text-xs text-lightGrey w-[10rem] sm:w-[20rem]">
+        <p className="w-[10rem] text-xs text-lightGrey sm:w-[20rem]">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pharetra
           adipiscing ultrices vulputate posuere tristique. In sed odio nec
           aliquet eu proin mauris et
@@ -31,14 +32,14 @@ const AboutUs = () => (
         <img
           src={images.knife}
           alt="knife"
-          className="h-[25rem] mx-2 w-[3rem] sm:h-[25rem] sm:w-[3rem]"
+          className="mx-2 h-[25rem] w-[3rem] sm:h-[25rem] sm:w-[3rem]"
         />
       </div>
 
-      <div className="flex-1 flex items-start justify-end text-left flex-col">
+      <div className="flex flex-1 flex-col items-start justify-end text-left">
         <h3 className="p-cormorant text-golden">Our History</h3>
         <img src={images.spoon} alt="spoon img" className="spoon-img mb-4" />
-        <p className="text-xs text-lightGrey  w-[10rem] sm:w-[20rem]">
+        <p className="w-[10rem] text-xs  text-lightGrey sm:w-[20rem]">
           Adipiscing tempus ullamcorper lobortis odio tellus arcu volutpat.
           Risus placerat morbi volutpat habitasse interdum mi aliquam In sed
           odio nec aliquet
