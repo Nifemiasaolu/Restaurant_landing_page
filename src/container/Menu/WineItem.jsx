dd320674d955a89bcc1d28f869b60c3d1f0dea79
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-11-19 22:47:02 +0100

    Tweak on style

--- src/container/Menu/WineItem.jsx
+++ src/container/Menu/WineItem.jsx
@@ -2,9 +2,16 @@ function WineItem({ wine }) {
   return (
     <li className="mt-4">
       <div className="flex items-center justify-between">
+        <div className="w-[100%]">
+
         <p className="text-golden">{wine.title}</p>
+        </div>
+
+        <div className="w-[100%]">
+
 
         <div className="mx-4 h-[1px] w-[40px] bg-golden " />
+        </div>
 
         <div className="text-white ">{wine.price}</div>
       </div>
