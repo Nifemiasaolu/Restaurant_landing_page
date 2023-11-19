a2a164e6810ece8e0c9f09782e67af7be09d4d59
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-11-19 22:23:16 +0100

    Reusable component

--- src/components/SubHeading/SubHeading.jsx
+++ src/components/SubHeading/SubHeading.jsx
@@ -7,14 +7,14 @@ const SubHeading = ({ title, children, type }) => {
 
   const style = {
     normal: base + ``,
-    flex: base + `flex-center text-lightGrey`,
+    flex: base + `flex-center text-lightGrey text-center`,
   };
 
   return (
     <div className={style[type]}>
       <p >{title}</p>
       <img src={images.spoon} alt="spoon" className="spoon-img mb-4 mt-1" />
-      <p className="text-lg text-golden">{children}</p>
+      <p className="text-lg text-golden mb-8">{children}</p>
     </div>
   );
 };
