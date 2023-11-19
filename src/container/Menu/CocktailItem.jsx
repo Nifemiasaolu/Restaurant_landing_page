32b63cc5c8c12fc8b89ec55cb504358b3cd76ca2
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-11-19 22:46:41 +0100

    Tweak on style

--- src/container/Menu/CocktailItem.jsx
+++ src/container/Menu/CocktailItem.jsx
@@ -2,8 +2,18 @@ function CocktailItem({ cocktail }) {
   return (
     <li className="mt-4">
       <div className="flex items-center justify-between">
+      <div className="w-[100%]">
+
+
         <p className="text-golden">{cocktail.title}</p>
+      </div>
+      
+<div className="w-[100%]">
+
+
         <div className="w-[40px] h-[1px] bg-golden mx-4 " />
+   
+</div>
         <div className="text-white">{cocktail.price}</div>
       </div>
 
