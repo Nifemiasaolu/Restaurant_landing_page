3bddcfb384c6664c0a470d58ad2ad49ca518ce8d
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-11-22 14:52:06 +0100

    Laurel page details

--- src/container/Laurels/Laurels.jsx
+++ src/container/Laurels/Laurels.jsx
@@ -6,23 +6,27 @@ import AwardItem from "./AwardItem";
 
 const Laurels = () => {
   return (
-    <div id="awards" className="app-bg app-wrapper flex flex-col section-padding ">
+    <div
+      id="awards"
+      className="app-bg section-padding flex flex-col sm:flex-row "
+    >
       <div className="app-wrapper-info">
         <SubHeading title="Awards & recognition" type="normal">
           Our Laurels
         </SubHeading>
 
-        <ul className="mt-[1rem] flex flex-wrap items-center justify-start">
+        <ul className="mb-[3rem] mt-[1rem] w-[100%] flex flex-wrap items-start justify-start gap-8 sm:flex-col">
           {data.awards.map((award) => (
             <AwardItem award={award} key={award.title} />
           ))}
         </ul>
       </div>
-      <div className="app-wrapper-img w-full">
+
+      <div className="app-wrapper-img ml-[0]  w-full">
         <img
           src={images.laurels}
           alt="laurels"
-          className="aspect-square w-1/2"
+          className="aspect-square"
         />
       </div>
     </div>
