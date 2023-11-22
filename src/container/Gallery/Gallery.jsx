85954c04cefebe2b59aaa56ec5973fb47d63acf1
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-11-22 14:51:47 +0100

    Gallery structural work

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React from "react";
 
-import './Gallery.css';
+import { images, data } from "../../constants";
+import { SubHeading } from "../../components";
+import {
+  BsInstagram,
+  BsArrowLeftShort,
+  BsArrowRightShort,
+} from "react-icons/bs";
+import Button from "../../ui/Button";
 
-const Gallery = () => (
-  <div>
-    Gallery
-  </div>
-);
-
+const Gallery = () => {
+  return (
+    <div className="flex-center">
+      <div>
+        <SubHeading title="instagram" type="normal">
+          Photo Gallery{" "}
+        </SubHeading>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus quae
+          odit dolorum fuga dolores. Esse quo cupiditate iusto nulla fugit?
+        </p>
+        <Button type="small">View More</Button>
+      </div>
+    </div>
+  );
+};
 export default Gallery;
