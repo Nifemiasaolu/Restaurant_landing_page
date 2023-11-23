88fd43590a6262ca71e305d1b99b416b4496b212
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-11-23 14:36:41 +0100

    Footer details

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,11 +1,33 @@
-import React from 'react';
+import React from "react";
 
-import './Footer.css';
+import { images } from "../../constants";
+import { SubHeading } from "../../components";
+import Button from "../../ui/Button";
 
-const Footer = () => (
-  <div>
-    Footer
-  </div>
-);
+const Footer = () => {
+  return (
+    <div
+      className="flex-center flex-col bg-black 
+   p-[6rem] md:flex-row"
+    >
+      <div>
+        <SubHeading title="newsletter" type="normal">
+          subscribe to our newsletter
+        </SubHeading>
+        <p className="text-xs text-lightGrey">And never miss latest Updates</p>
+      </div>
+
+      <div className="my-4 flex flex-col md:flex-row">
+        <input
+          type="text"
+          id="newsletter"
+          placeholder="Email Address"
+          className="px-4 py-1 rounded-md bg-black border text-white border-lightGrey placeholder:text-xs placeholder:text-lightGrey"
+        />
+        <Button type="small">Subscribe</Button>
+      </div>
+    </div>
+  );
+};
 
 export default Footer;
