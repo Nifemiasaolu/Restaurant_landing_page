b4cbafcbdf112da573776fd4ae79afae14434e94
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-11-24 21:54:06 +0100

    Tweak on styles

--- src/components/SubHeading/SubHeading.jsx
+++ src/components/SubHeading/SubHeading.jsx
@@ -6,16 +6,17 @@ const SubHeading = ({ title, children, type }) => {
   const base = `p-cormorant text-xs md:text-sm flex flex-col `;
 
   const style = {
-    normal: base + ``,
+    normal: base + `mb-6`,
     flex: base + `flex-center text-lightGrey text-center`,
-    // large: base + `text-lg`
+    subscribe: base + `flex-center text-lightGrey text-center 
+    leading-7 sm:leading-3 mb-3 `
   };
 
   return (
     <div className={style[type]}>
       <p>{title}</p>
       <img src={images.spoon} alt="spoon" className="spoon-img mb-4 mt-1" />
-      <h1 className="mb-8 text-[20px] text-golden">{children}</h1>
+      <h1 className=" text-[20px] text-golden">{children}</h1>
     </div>
   );
 };
