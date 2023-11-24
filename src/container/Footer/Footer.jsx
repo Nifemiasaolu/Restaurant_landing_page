4fa0106a1674ad9010775f5a726cb5553a4fb368
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-11-24 21:59:27 +0100

    Footer stage set

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -6,22 +6,25 @@ import Button from "../../ui/Button";
 
 const Footer = () => {
   return (
-    <div className="flex-center flex-col bg-black p-[6rem]">
+    <div className="flex-center flex-col bg-black section-padding ">
       <div className="flex-center flex-col ">
-        <SubHeading title="newsletter" type="flex">
+        <SubHeading title="newsletter" type="subscribe">
           subscribe to our newsletter
         </SubHeading>
         <p className="text-xs text-lightGrey">And never miss latest Updates</p>
       </div>
 
-      <div className="my-4 flex  flex-col md:flex-row md:gap-4">
-        <input
-          type="text"
-          id="newsletter"
-          placeholder="Email Address"
-          className="rounded-md border border-lightGrey bg-black px-4 py-1 text-lightGrey placeholder:text-xs placeholder:text-lightGrey"
-        />
-        <Button type="small">Subscribe</Button>
+      <div className="my-4 flex flex-col  md:flex-row md:gap-4">
+        <div>
+          <input
+            type="text"
+            id="newsletter"
+            placeholder="Email Address"
+            className="rounded-md border border-lightGrey bg-black
+             px-4 py-1 text-lightGrey placeholder:text-xs placeholder:text-lightGrey"
+          />
+        </div>
+        <Button type="footer">Subscribe</Button>
       </div>
     </div>
   );
