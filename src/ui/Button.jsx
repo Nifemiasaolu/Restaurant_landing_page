ccb2e1454180984c6e006d9c536cbe1460c687c8
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-11-24 21:59:41 +0100

    Lil style on button component

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -8,6 +8,7 @@ function Button({ children, type }) {
   const styles= {
     primary: base + `px-4 py-1 lg:text-base lg:leading-[20px]`,
     small: base + `px-3 text-xs mt-4 rounded-[3px]`,
+    footer: base + `px-3 text-xs mt-4 rounded-[3px] md:mt-0`,
   } 
 
   return (
