d7605eb99f65b861b624deb0d013c3843c3cfaad
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-11-29 09:03:06 +0100

    Lil detail change

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,23 +1,16 @@
-
 function Button({ children, type }) {
   const base = `bg-golden font-['Cormont_Upright'] rounded-[4px] 
   cursor-pointer border-none  text-base 
   font-bold leading-7 tracking-wider text-black 
-  outline-none `
+  outline-none `;
 
-  const styles= {
+  const styles = {
     primary: base + `px-4 py-1 lg:text-base lg:leading-[20px]`,
     small: base + `px-3 text-xs mt-4 rounded-[3px]`,
     footer: base + `px-3 text-xs mt-4 rounded-[3px] md:mt-0`,
-  } 
+  };
 
-  return (
-    <button
-      className={styles[type]}
-    >
-      {children}
-    </button>
-  );
+  return <button className={styles[type]}>{children}</button>;
 }
 
 export default Button;
