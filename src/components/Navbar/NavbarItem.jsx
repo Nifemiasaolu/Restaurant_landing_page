db8c15e95e14169471725d8d97463003a2a58fa4
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-11-29 09:06:20 +0100

    lil tweak

--- src/components/Navbar/NavbarItem.jsx
+++ src/components/Navbar/NavbarItem.jsx
@@ -1,12 +1,12 @@
-function NavbarItem({ item, onCLick }) {
+function NavbarItem({ item, onClick }) {
   return (
     <li
       className="p-opensans m-4 cursor-pointer font-['Cormorant_Upright']
      text-golden hover:text-white sm:mr-2 sm:font-['Open_sans']
       sm:text-white sm:hover:text-lightGrey sm:text-xs lg:text-sm"
-      onClick={onCLick}
+      onClick={onClick}
     >
-      <a href={item.id}>{item.title}</a>
+      <a href={item.id} >{item.title}</a>
     </li>
   );
 }
