3b3eec4161ff03f67ed10747e5cdef99541b9c77
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-11-29 09:01:58 +0100

    Modal logic

--- src/components/Navbar/NavbarItem.jsx
+++ src/components/Navbar/NavbarItem.jsx
@@ -1,9 +1,10 @@
-function NavbarItem({ item }) {
+function NavbarItem({ item, onCLick }) {
   return (
     <li
       className="p-opensans m-4 cursor-pointer font-['Cormorant_Upright']
      text-golden hover:text-white sm:mr-2 sm:font-['Open_sans']
       sm:text-white sm:hover:text-lightGrey sm:text-xs lg:text-sm"
+      onClick={onCLick}
     >
       <a href={item.id}>{item.title}</a>
     </li>
