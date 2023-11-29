68dd0d1a0450aa689cd5083bc3d0fae5b5559da1
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-11-29 09:07:42 +0100

    Adjustment on details

--- src/components/Navbar/NavbarItem.jsx
+++ src/components/Navbar/NavbarItem.jsx
@@ -4,9 +4,9 @@ function NavbarItem({ item, onClick }) {
       className="p-opensans m-4 cursor-pointer font-['Cormorant_Upright']
      text-golden hover:text-white sm:mr-2 sm:font-['Open_sans']
       sm:text-white sm:hover:text-lightGrey sm:text-xs lg:text-sm"
-      onClick={onClick}
+     
     >
-      <a href={item.id} >{item.title}</a>
+      <a href={item.id} onClick={onClick}>{item.title}</a>
     </li>
   );
 }
